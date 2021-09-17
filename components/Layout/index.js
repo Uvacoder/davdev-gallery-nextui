536c3229b0d15bbb3ac3cc92943821b6f73a8680
Jonathan García <jona03g97@gmail.com>
2021-09-17 17:23:50 -0600

    Styled Layout

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,14 +1,21 @@
-import { Text } from "@nextui-org/react";
-import styles from "./style";
+import { Text, Spacer } from '@nextui-org/react'
+import styles, { globals } from './style'
 
 export default function Layout({ children }) {
   return (
     <>
       <main>
-        <Text h1>Davdev Gallery</Text>
+        <Spacer y={1} inline={true} />
+        <Text color='white' align='center' size='4.5rem' h1>
+          Davdev Gallery
+        </Text>
+        <Spacer y={1} />
         {children}
       </main>
       <style jsx>{styles}</style>
+      <style jsx global>
+        {globals}
+      </style>
     </>
-  );
+  )
 }
--- components/Layout/style.js
+++ components/Layout/style.js
@@ -1,16 +1,30 @@
-import css from "styled-jsx/css";
+import css from 'styled-jsx/css'
 
 export const globals = css.global`
+  @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@100;300;700&display=swap');
+
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
-`;
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-family: 'Raleway', sans-serif;
+  }
+`
 
 export default css`
   main {
-    background-color: black;
+    background: linear-gradient(to right, #6441a5, #2a0845);
     min-height: 100vh;
   }
-`;
+
+  h1 {
+    font-weight: 700;
+  }
+`
