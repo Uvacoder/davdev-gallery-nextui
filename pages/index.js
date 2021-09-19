58d2cf005dd924bbbe71151c4a88c9e864d03e18
Jonathan García <jona03g97@gmail.com>
2021-09-19 12:01:48 -0600

    With container

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { Container, Row, Col, Spacer, Text, Button } from '@nextui-org/react'
+import { Camera } from 'react-iconly'
 import mockImages from '../mockImages'
 import Head from 'next/head'
 
@@ -9,43 +10,54 @@ export default function Home() {
         <meta name='description' content='App de galería con Nextjs y NextUI' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <section>
-        <Container>
-          <Row wrap='wrap'>
-            {mockImages.map((image) => {
-              return (
-                <>
-                  <Col key={image._id} span={4}>
-                    <img src={image.url} alt={image.title} />
-                    <Text color='white' h2>
-                      <p className='title'>{image.title}</p>
-                    </Text>
-                    <Spacer y={0.1} />
-                    <Text color='white'>
-                      <p>{image.description}</p>
-                    </Text>
-                    <Spacer y={0.3} />
-                    <Button flat color='primary' auto>
-                      Ver imagen
-                    </Button>
-                    <Button flat color='error' auto>
-                      Eliminar
-                    </Button>
-                    <Spacer y={2} />
-                  </Col>
-                </>
-              )
-            })}
-          </Row>
-        </Container>
-      </section>
-      <style jsx>{`
-        section {
-          border: 1px solid white;
-        }
+      <Container>
+        <Row>
+          <Col>
+            <Button
+              flat
+              color='success'
+              auto
+              iconRight={<Camera fill='white' />}
+            >
+              Nueva imagen
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+      <Spacer y={1} />
+      <Container xs display='flex'>
+        <Row wrap='wrap' gap={0.5} display='flex'>
+          {mockImages.map((image) => {
+            return (
+              <>
+                <Col key={image._id} span={4}>
+                  <img src={image.url} alt={image.title} />
+                  <Text color='white' h2>
+                    <p className='title'>{image.title}</p>
+                  </Text>
+                  <Spacer y={0.1} />
+                  <Text color='white'>
+                    <p>{image.description}</p>
+                  </Text>
+                  <Spacer y={0.3} />
+                  <Button flat color='secondary' auto>
+                    Ver imagen
+                  </Button>
+                  <Spacer y={0.3} />
+                  <Button flat color='error' auto>
+                    Eliminar
+                  </Button>
+                  <Spacer y={0.5} />
+                </Col>
+              </>
+            )
+          })}
+        </Row>
+      </Container>
 
+      <style jsx>{`
         img {
-          width: 95%;
+          width: 100%;
         }
       `}</style>
     </>
