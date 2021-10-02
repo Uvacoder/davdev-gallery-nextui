6ddd96b4a43eb22b74574be492acb5190be53ece
Jonathan García <jona03g97@gmail.com>
2021-10-01 21:07:09 -0600

    Add real API

--- pages/[id]/delete.js
+++ pages/[id]/delete.js
@@ -1,47 +1,47 @@
-import { Text, Button, Container, Spacer } from "@nextui-org/react";
-import axios from "axios";
-import router from "next/router";
+import { Text, Button, Container, Spacer } from '@nextui-org/react'
+import axios from 'axios'
+import router from 'next/router'
 
 export default function Delete({ id, title }) {
   const handleDelete = async () => {
-    await axios.delete(`${process.env.NEXT_PUBLIC_API_URL_LOCAL}/${id}`);
-    router.push("/");
-  };
+    await axios.delete(`${process.env.NEXT_PUBLIC_API_URL}/${id}`)
+    router.push('/')
+  }
 
   const handleCancel = () => {
-    router.push("/");
-  };
+    router.push('/')
+  }
 
   return (
     <>
       <Text
         h2
-        color="error"
-        align="center"
+        color='error'
+        align='center'
       >{`¿Seguro que quiere borrar la imagen "${title}"?`}</Text>
       <Spacer y={0.5} />
-      <Container align="center">
-        <Button.Group color="error" flat>
+      <Container align='center'>
+        <Button.Group color='error' flat>
           <Button onClick={handleCancel}>Cancelar</Button>
           <Button onClick={handleDelete}>Eliminar</Button>
         </Button.Group>
       </Container>
     </>
-  );
+  )
 }
 
 export const getServerSideProps = async (ctx) => {
-  const { query } = ctx;
-  const { id } = query;
+  const { query } = ctx
+  const { id } = query
 
   const { data: image } = await axios.get(
-    `${process.env.NEXT_PUBLIC_API_URL_LOCAL}/${id}`
-  );
+    `${process.env.NEXT_PUBLIC_API_URL}/${id}`
+  )
 
   return {
     props: {
       id: image._id,
-      title: image.title,
-    },
-  };
-};
+      title: image.title
+    }
+  }
+}
--- pages/new.js
+++ pages/new.js
@@ -1,6 +1,6 @@
-import Link from "next/link";
-import router from "next/router";
-import { ChevronLeftCircle } from "react-iconly";
+import Link from 'next/link'
+import router from 'next/router'
+import { ChevronLeftCircle } from 'react-iconly'
 import {
   Text,
   Link as UILink,
@@ -9,49 +9,49 @@ import {
   Col,
   Row,
   Button,
-  Input,
-} from "@nextui-org/react";
-import useField from "../hooks/useField";
-import axios from "axios";
+  Input
+} from '@nextui-org/react'
+import useField from '../hooks/useField'
+import axios from 'axios'
 
 export default function New() {
   const titleField = useField({
-    type: "text",
-    placeholder: "Título",
-    require: "true",
-  });
+    type: 'text',
+    placeholder: 'Título',
+    require: 'true'
+  })
 
   const descriptionField = useField({
-    type: "text",
-    placeholder: "Título",
-    require: "true",
-  });
+    type: 'text',
+    placeholder: 'Título',
+    require: 'true'
+  })
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
     try {
-      const fd = new FormData();
-      fd.append("title", titleField.value);
-      fd.append("description", descriptionField.value);
-      fd.append("uploadImage", e.target[0].files[0]);
-      await axios.post(process.env.NEXT_PUBLIC_API_URL_LOCAL, fd);
-      router.push("/");
+      const fd = new FormData()
+      fd.append('title', titleField.value)
+      fd.append('description', descriptionField.value)
+      fd.append('uploadImage', e.target[0].files[0])
+      await axios.post(process.env.NEXT_PUBLIC_API_URL, fd)
+      router.push('/')
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
   return (
     <>
       <div>
-        <Text h2 color="white">
+        <Text h2 color='white'>
           <p>Ingresa los datos de la nueva imagen.</p>
         </Text>
         <Spacer y={0.5} />
-        <Link href="/">
-          <UILink color="success">
+        <Link href='/'>
+          <UILink color='success'>
             <article>
-              <ChevronLeftCircle set="bold" />
+              <ChevronLeftCircle set='bold' />
               <p>Ir a inicio</p>
             </article>
           </UILink>
@@ -62,32 +62,32 @@ export default function New() {
             <form onSubmit={handleSubmit}>
               <Row>
                 <label>
-                  <Text color="white">Seleccionar imagen</Text>
-                  <input type="file" />
+                  <Text color='white'>Seleccionar imagen</Text>
+                  <input type='file' />
                 </label>
               </Row>
               <Spacer y={1.4} />
               <Row>
                 <Input
-                  labelPlaceholder="Título"
-                  status="primary"
-                  size="medium"
-                  width="100%"
+                  labelPlaceholder='Título'
+                  status='primary'
+                  size='medium'
+                  width='100%'
                   {...titleField}
                 />
               </Row>
               <Spacer y={2} />
               <Row>
                 <Input
-                  labelPlaceholder="Descripción"
-                  status="primary"
-                  width="100%"
+                  labelPlaceholder='Descripción'
+                  status='primary'
+                  width='100%'
                   {...descriptionField}
                 />
               </Row>
               <Spacer y={0.8} />
               <Row>
-                <Button flat color="success" size="large" type="submit">
+                <Button flat color='success' size='large' type='submit'>
                   Subir
                 </Button>
               </Row>
@@ -115,7 +115,7 @@ export default function New() {
           color: white;
         }
 
-        input[type="file"]::-webkit-file-upload-button {
+        input[type='file']::-webkit-file-upload-button {
           border: 1px solid grey;
           background: #1b314c;
           font-size: 1rem;
@@ -125,5 +125,5 @@ export default function New() {
         }
       `}</style>
     </>
-  );
+  )
 }
